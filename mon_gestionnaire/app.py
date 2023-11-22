6351e86f8dc956197d3d179044228f55a925cb9c
Lotfi <lotfi.hadbi@epita.fr>
2023-11-22 14:41:23 +0100

    a

--- mon_gestionnaire/app.py
+++ mon_gestionnaire/app.py
@@ -4,8 +4,14 @@ import password_manager_backend as backend
 app = Flask(__name__)
 app.secret_key = 'une_clé_secrète_ici'
 
+# Création et configuration de la connexion à la base de données
+conn, db_error = backend.create_connection('votre_base_de_donnees.db')
+if db_error:
+    raise Exception(f"Erreur de connexion à la base de données: {db_error}")
+
 @app.route('/')
 def index():
+    # Si l'utilisateur est déjà connecté, redirigez-le vers le tableau de bord
     if 'username' in session:
         return redirect(url_for('dashboard'))
     return render_template('login.html')
@@ -14,27 +20,33 @@ def index():
 def login():
     username = request.form['username']
     password = request.form['password']
-    cipher_suite, error = backend.validate_login(username, password)
+    cipher_suite, error = backend.login(conn, username, password)
     if cipher_suite:
         session['username'] = username
         return redirect(url_for('dashboard'))
     else:
-        flash(error or "Nom d'utilisateur ou mot de passe incorrect.")
+        flash("Nom d'utilisateur ou mot de passe incorrect.")
         return redirect(url_for('index'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if 'username' in session:
+        # Rediriger vers le tableau de bord si l'utilisateur est déjà connecté
         return redirect(url_for('dashboard'))
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+
+        # Appeler la fonction d'enregistrement du backend
         success, error = backend.register_user(username, password)
+
         if success:
             flash("Inscription réussie. Veuillez vous connecter.")
             return redirect(url_for('index'))
         else:
             flash(error or "Une erreur est survenue lors de l'inscription.")
+
     return render_template('register.html')
 
 @app.route('/dashboard')
@@ -42,7 +54,6 @@ def dashboard():
     if 'username' not in session:
         flash("Veuillez vous connecter pour accéder au tableau de bord.")
         return redirect(url_for('index'))
-    # Ajouter ici la logique pour afficher les informations de l'utilisateur
     return render_template('dashboard.html')
 
 @app.route('/logout')
