7326e26b7d11e1dbaa0c0263c03b77bf31b741e4
Lotfi <lotfi.hadbi@epita.fr>
2023-11-24 17:00:43 +0100

    Remooving pycache

--- mon_gestionnaire/models.py
+++ mon_gestionnaire/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(120), nullable=False)
-    key = db.Column(db.String(255), nullable=False)  # Clé de chiffrement pour les mots de passe
+    key = db.Column(db.String)  # Clé de chiffrement pour les mots de passe
 
     def __init__(self, username, password):
         self.username = username
