f481c25b08be84ff4a3be6307e1e55bb217ca009
Lotfi <lotfi.hadbi@epita.fr>
2023-11-26 16:32:13 +0100

    Modification de l'interface utilisateur

--- mon_gestionnaire/schema.sql
+++
@@ -1,7 +0,0 @@
--- Crée la table users si elle n'existe pas déjà
-CREATE TABLE IF NOT EXISTS users (
-    -- Définition de la colonne 'id'
-    id INTEGER PRIMARY KEY AUTOINCREMENT, -- Un identifiant unique pour chaque utilisateur, auto-incrémenté
-    username TEXT UNIQUE NOT NULL, -- Le nom d'utilisateur, doit être unique et non nul
-    password TEXT NOT NULL -- Le mot de passe, requis et ne peut pas être nul
-);
